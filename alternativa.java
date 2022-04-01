f55a0f37e93c357f17ccdab1cbc2009eb87073c9
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-01 20:12:07 -0300

    Create alternativa.java

---
+++ alternativa.java
@@ -0,0 +1,24 @@
+Scanner entrada = new Scanner (System.in);
+    System.out.print("Pergunta ?");
+
+    char responda = entrada.next().charAt(0);
+
+    switch(responda){
+      case 'a' :
+        System.out.print("errado" );
+        break;
+      case 'b' :
+        System.out.print("errado" );
+        break;
+      case 'c' :
+        System.out.print("certo" );
+        break;
+      case 'd':
+        System.out.print("errado" );
+        break;
+      case 'e' :
+        System.out.print("errado");
+        break;
+      default:
+        System.out.print("Não tem esta alternativa");
+        break;
